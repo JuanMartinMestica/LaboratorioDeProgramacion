70f45019432d10c987494041e8c21144e63e903d
JuanMartinMestica <juan.mestica@est.fi.uncoma.edu.ar>
2021-08-30 20:37:22 -0300

    Interfaz creada, definición de métodos.

---
+++ src/laboratorio/ItemArchivo.java
@@ -0,0 +1,13 @@
+package laboratorio;
+
+/*Laboratorio de Programación - Fernando Iraira - Juan Mestica*/
+
+/*Interfaz: ItemArchivo*/
+
+public interface ItemArchivo {
+    
+    public double getPeso();
+    public String getNombre();
+    public void mostrar();
+    
+}
