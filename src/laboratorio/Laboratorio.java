1a6d2c38fe741e6e23ca2b72b4cce67647be46f8
JuanMartinMestica <juan.mestica@est.fi.uncoma.edu.ar>
2021-08-31 18:49:57 -0300

    Implementación de la carga inicial de archivos y carpetas

--- src/laboratorio/Laboratorio.java
+++ src/laboratorio/Laboratorio.java
@@ -6,27 +6,58 @@ public class Laboratorio {
 
     public static void main(String[] args) {
 
-        
-        /*Pruebas con archivos*/
-        
-        String path = "C:\\Users\\MARTIN\\Desktop\\Segundo cuatrimestre 2021";
+        cargaInicial();
 
-        File archivo = new File(path, "Redes");
+    }
 
-    
-          if (archivo.exists()) {
-            
-              System.out.println("!!!!");
-              
-              for (File ar: archivo.listFiles()) { 
-                 System.out.println("Archivo: " + ar.getName() + " pesa: " + ar.length());
-                       
-              }
-              
+    public static void cargaInicial() {
+
+        //Se obtiene la ruta de la carpeta padre
+        String path = "C:\\Users\\MARTIN\\Desktop";
+
+        //Se obtiene un item de tipo File, que será una carpeta o archivo
+        File item = new File(path, "Prueba");
+
+        //Se verifica si existe el item
+        if (item.exists()) {
+
+            Carpeta principal = new Carpeta(item.getName());
+
+            cargaRecursiva(principal, item);
+
+            System.out.println(principal);
+
+        } else {
+            System.out.println("ERROR: No existe archivo-carpeta en la ruta dada.");
         }
-           
-    
 
+    }
+
+    private static void cargaRecursiva(Carpeta x, File item) {
+
+        //Si es una carpeta
+        if (item.isDirectory()) {
+
+            for (File items : item.listFiles()) {
+
+                if (items.isDirectory()) {
+
+                    Carpeta nuevaCarpeta = new Carpeta(items.getName());
+                    x.añadirItem(nuevaCarpeta);
+                }
+
+                cargaRecursiva(x, items);
+                
+            }
+        } else {
+
+            //Si es un archivo, se crea una instancia de archivo
+            Archivo nuevoArchivo = new Archivo(item.getName(), item.length());
+
+            //Se agrega el archivo a la carpeta que lo contiene
+            x.añadirItem(nuevoArchivo);
+
+        }
     }
 
 }
