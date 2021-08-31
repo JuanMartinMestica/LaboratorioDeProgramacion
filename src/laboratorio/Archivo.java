039877ab676b0dfd47c8bc95076fad7873ea3e30
JuanMartinMestica <juan.mestica@est.fi.uncoma.edu.ar>
2021-08-31 18:19:00 -0300

    implementación toString

--- src/laboratorio/Archivo.java
+++ src/laboratorio/Archivo.java
@@ -1,17 +1,16 @@
 package laboratorio;
 
 /*Laboratorio de Programación - Fernando Iraira - Juan Mestica*/
+public class Archivo implements ItemArchivo {
 
-public class Archivo implements ItemArchivo{
-    
     String nombre;
     double peso;
-    
+
     public Archivo(String nombre, double peso) {
         this.nombre = nombre;
         this.peso = peso;
     }
-   
+
     @Override
     public double getPeso() {
         return this.peso;
@@ -21,5 +20,9 @@ public class Archivo implements ItemArchivo{
     public String getNombre() {
         return this.nombre;
     }
-    
+
+    @Override
+    public String toString() {
+        return "Nombre archivo: " + this.nombre + "| Peso: " + this.peso;
+    }
 }
