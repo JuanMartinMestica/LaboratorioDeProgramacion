8be84fbdbcd66d33982819ff7fc45b246da3e046
JuanMartinMestica <juan.mestica@est.fi.uncoma.edu.ar>
2021-08-30 21:44:59 -0300

    Pruebas con archivos

--- src/laboratorio/Laboratorio.java
+++ src/laboratorio/Laboratorio.java
@@ -1,9 +1,32 @@
 package laboratorio;
 
+import java.io.File;
+
 public class Laboratorio {
 
     public static void main(String[] args) {
-        // TODO code application logic here
-    }
+
+        
+        /*Pruebas con archivos*/
+        
+        String path = "C:\\Users\\MARTIN\\Desktop\\Segundo cuatrimestre 2021";
+
+        File archivo = new File(path, "Redes");
+
     
+          if (archivo.exists()) {
+            
+              System.out.println("!!!!");
+              
+              for (File ar: archivo.listFiles()) { 
+                 System.out.println("Archivo: " + ar.getName() + " pesa: " + ar.length());
+                       
+              }
+              
+        }
+           
+    
+
+    }
+
 }
