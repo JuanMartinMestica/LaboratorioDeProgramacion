04bdae55cd001452f9ec064746cc714dce383892
Fernando <Fernando@DESKTOP-98630AI>
2021-09-22 12:51:27 -0300

    Se eliminaron metodos sin usar

--- src/laboratorio/Laboratorio.java
+++ src/laboratorio/Laboratorio.java
@@ -41,30 +41,6 @@ public class Laboratorio {
         }
     }
 
-    public static Carpeta cargaInicial() {
-
-        Carpeta raiz = null;
-
-        //Se especifica la ruta de la carpeta padre
-        String path = "C:\\Users\\Fernando\\Desktop";
-
-        //Se crea un item de tipo File, que será una carpeta o archivo
-        File item = new File(path, "Prueba");
-
-        //Se verifica si existe el item
-        if (item.exists()) {
-
-            System.out.println("Se esta escaneando la carpeta, por favor espere...");
-            raiz = cargaPublica(item);
-            System.out.println("Escaneo completo. \n\n");
-        } else {
-            System.out.println("ERROR: No existe archivo-carpeta en la ruta dada.");
-        }
-
-        return raiz;
-
-    }
-
     private static int mostrarMenu() {
 
         int opcion;
@@ -81,50 +57,4 @@ public class Laboratorio {
         return opcion;
 
     }
-
-    private static Carpeta accederSubcarpeta(Carpeta carpetaActual) {
-
-        String nombreCarpeta;
-
-        System.out.print("Ingrese el nombre de la carpeta a la que quiere ingresar: ");
-        nombreCarpeta = TecladoIn.readLine();
-
-        Carpeta subcarpeta = carpetaActual.obtenerSubcarpeta(nombreCarpeta);
-
-        return subcarpeta;
-    }
-
-    private static Carpeta cargaPublica(File item) {
-        Carpeta raiz;
-
-        raiz = new Carpeta(item.getName());
-        cargaAux(raiz, item);
-
-        return raiz;
-    }
-
-    private static void cargaAux(Carpeta visitado, File item) {
-
-        for (File subElem : item.listFiles()) {
-
-            //Si el subelemento es una carpeta, se crea una instancia de Carpeta
-            if (subElem.isDirectory()) {
-
-                Carpeta nuevaCarpeta = new Carpeta(subElem.getName());
-
-                cargaAux(nuevaCarpeta, subElem);
-
-                //Se añade la Carpeta a la Carpeta padre
-                visitado.añadirItem(nuevaCarpeta);
-
-            } else {
-                //Si el subelemento es un archivo, se crea una instancia de Archivo
-                Archivo nuevoArchivo = new Archivo(subElem.getName(), subElem.length());
-
-                //Se añade el Archivo a la Carpeta
-                visitado.añadirItem(nuevoArchivo);
-            }
-        }
-    }
-
 }
