6212d4a0b089afc438dfe84c87b9b2cf34995e8d
Fernando <Fernando@DESKTOP-98630AI>
2021-09-22 13:48:23 -0300

    Se quitaron colores que no se usaban

--- src/laboratorio/Explorador.java
+++ src/laboratorio/Explorador.java
@@ -18,8 +18,6 @@ public class Explorador {
     //Variables globales para prints
     public static final String ANSI_RED = "\u001B[31m";
     public static final String ANSI_RESET = "\u001B[0m";
-    public static final String ANSI_GREEN = "\u001B[32m";
-    public static final String ANSI_PURPLE = "\u001B[35m";
 
     Pila visitados;
     Carpeta carpetaActual;
