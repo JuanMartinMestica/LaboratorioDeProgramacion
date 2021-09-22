6a44c1a5ec1e0bc74ff4d0379a487f8f91bd8c65
Fernando <Fernando@DESKTOP-98630AI>
2021-09-22 13:47:22 -0300

    Se quitaron colores que no se usaban

--- src/laboratorio/Laboratorio.java
+++ src/laboratorio/Laboratorio.java
@@ -7,7 +7,6 @@ public class Laboratorio {
     //Variables globales para prints
     public static final String ANSI_RED = "\u001B[31m";
     public static final String ANSI_RESET = "\u001B[0m";
-    public static final String ANSI_GREEN = "\u001B[32m";
     public static final String ANSI_PURPLE = "\u001B[35m";
 
     public static void main(String[] args) {
