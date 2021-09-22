0e5428ddc3ed83ed92ab6964c55493c2999ac00d
Fernando <Fernando@DESKTOP-98630AI>
2021-09-22 12:22:23 -0300

    Creacion de la clase Explorador

---
+++ src/laboratorio/Explorador.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package laboratorio;
+
+import Utilidades.Pila;
+
+/**
+ *
+ * @author Fernando
+ */
+public class Explorador {
+
+    Pila visitados;
+    Carpeta carpetaActual;
+
+}
