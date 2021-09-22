e948b9b452846efee477c99c7662d6c9c773df8b
Fernando <Fernando@DESKTOP-98630AI>
2021-09-22 12:25:20 -0300

    Copiado de cargaInicial

--- src/laboratorio/Explorador.java
+++ src/laboratorio/Explorador.java
@@ -6,6 +6,7 @@
 package laboratorio;
 
 import Utilidades.Pila;
+import java.io.File;
 
 /**
  *
@@ -15,12 +16,31 @@ public class Explorador {
 
     Pila visitados;
     Carpeta carpetaActual;
+    Carpeta raiz;
 
     public Explorador() {
         visitados = new Pila();
     }
 
     public Carpeta cargaInicial() {
+
+        //Se especifica la ruta de la carpeta padre
+        String path = "C:\\Users\\Fernando\\Desktop";
+
+        //Se crea un item de tipo File, que será una carpeta o archivo
+        File item = new File(path, "Prueba");
+
+        //Se verifica si existe el item
+        if (item.exists()) {
+
+            System.out.println("Se esta escaneando la carpeta, por favor espere...");
+            raiz = cargaPublica(item);
+            System.out.println("Escaneo completo. \n\n");
+        } else {
+            System.out.println("ERROR: No existe archivo-carpeta en la ruta dada.");
+        }
+
+        return raiz;
 
     }
 }
